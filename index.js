5bad136e0a9b74302b4adc248c89e21b576b092d
borkarsaish65 <borkarsaish7@gmail.com>
2020-06-26 19:34:55 +0530

    26/06/2020

--- index.js
+++ index.js
@@ -1,27 +1,17 @@
-const child = document.querySelector('#child');
-const parent = document.querySelector('#parent');
-const grandparent = document.querySelector('#grandparent');
+const btn = document.querySelector('.button');
 
-
-
-//event listener for child div
-child.addEventListener('click',function(e){
+btn.addEventListener('drag',function(e){
     console.log(e);
-    e.stopPropagation();
-    console.log('child is clicked')
-},false);
+    console.log('hi')
+})
 
 
-//event listener for parent div
-parent.addEventListener('click',function(e){
-
-    console.log('parent is clicked')
-},false);
-
-
-
-//event listener for grandparent div
-grandparent.addEventListener('click',function(e){
+btn.addEventListener('focus',function(){
+    console.log('bye')
     console.log(e);
-    console.log('grandparent is clicked')
-},false);
+    e.addEventListener("keyup", event => {
+        console.log(event);
+        // do something
+      });
+})
+
