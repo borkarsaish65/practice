9c9c13483e7453bc52ce8310de63f45b23946f79
borkarsaish65 <borkarsaish7@gmail.com>
2020-07-01 19:14:24 +0530

    1-7-2020

--- index.js
+++ index.js
@@ -1,27 +1,33 @@
-let people = ['saish', 'Nikhil','Harsha','Tanish','Shivam'];
+function serverRequest(query, callback){
 
-
-
-people.forEach((pip)=>{
-
-
-  console.log(pip)
-})
-
-
-const myfunc =  (callBack) => {
-
-let num = 1;
-
-callBack(1)
-
-
+  console.log(query);
+  setTimeout(function(){
+    var response = query + "full!";
+    callback(response);
+  },5000);
 }
 
+function getResults(results){
+  console.log("Response from the server: " + results);
+}
 
-myfunc(function(value){
+serverRequest("The glass is half ", getResults);
+let value = 5;
+
+console.log(value);
+
+
+value = value + 199;
+
+console.log(value);
+
+
+value = 1;
+
+while(value < 100)
+{
+
+
   console.log(value)
-
-
-
-})
+value++
+}
