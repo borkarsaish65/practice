e694afa22c575b62afefb41f2a45c02fb39071b7
borkarsaish65 <borkarsaish7@gmail.com>
2020-07-04 09:26:10 +0530

    4-7-2020

--- index.js
+++ index.js
@@ -1,33 +1,8 @@
-function serverRequest(query, callback){
-
-  console.log(query);
-  setTimeout(function(){
-    var response = query + "full!";
-    callback(response);
-  },5000);
-}
-
-function getResults(results){
-  console.log("Response from the server: " + results);
-}
-
-serverRequest("The glass is half ", getResults);
-let value = 5;
-
-console.log(value);
-
-
-value = value + 199;
-
-console.log(value);
-
-
-value = 1;
-
-while(value < 100)
-{
-
-
-  console.log(value)
-value++
-}
+function insertAfter(referenceNode, newNode) {
+    referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+  }
+  
+  var el = document.createElement("span");
+  el.innerHTML = "test";
+  var div = document.getElementById("mainDiv");
+  insertAfter(div, el);
