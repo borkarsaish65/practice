6a0f618401e63a7577818bd3824939bdcfaeed9c
borkarsaish65 <borkarsaish7@gmail.com>
2020-07-04 19:12:42 +0530

    4-7-2020

--- index.js
+++ index.js
@@ -1,8 +1,34 @@
-function insertAfter(referenceNode, newNode) {
-    referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+var canvas = document.getElementById("canvas");
+var original = new MarvinImage();
+var image;
+
+original.load("1.png", function(){
+   // Draw the original image 
+   original.draw(canvas);
+
+
+   function edgeDetection2(){
+    image = original.clone();
+    image.clear(0xFF000000);
+    Marvin.prewitt(original, image);
+    Marvin.invertColors(image, image);
+    Marvin.thresholding(image, image, 200);
+    image.draw(canvas);
   }
   
-  var el = document.createElement("span");
-  el.innerHTML = "test";
-  var div = document.getElementById("mainDiv");
-  insertAfter(div, el);
+  
+   edgeDetection2()
+
+
+
+  function thresholding(){
+    image = original.clone();
+    Marvin.thresholding(original, image, 160);
+    image.draw(canvas);
+  }
+  
+  
+  //thresholding()
+});
+
+
