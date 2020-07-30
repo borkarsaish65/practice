76fb8a3b18a661cb35da1fbb445086e21506f7fc
borkarsaish65 <borkarsaish7@gmail.com>
2020-07-30 20:07:41 +0530

    30-7-2020 latest

---
+++ app.js
@@ -0,0 +1,8 @@
+var counter = require('./count');
+
+console.log(counter.counter(['Saish','Nikhil','Rahul',"saish"]));
+
+console.log(counter.addNum(10,20));
+
+console.log(counter.pi);
+
---
+++ count.js
@@ -0,0 +1,20 @@
+var counter = function (arr) {
+return "There are "+arr.length + " elements in the array";
+}
+
+var addNum = (m,n) =>{
+
+return 'The sum of the number is '+(m+n);
+
+}
+
+var pi = 3.14;
+
+
+module.exports  = {
+
+    counter:counter,
+    addNum:addNum,
+    pi:pi
+
+}
